3ccd93a72237fbe750f6e033807367100243b5cc
Jay Sharma <jay.indra.sharma@gmail.com>
2022-01-16 10:55:42 +0000

    Added medium and small tests

---
+++ tests/medium/test_stock_predictor.py
@@ -0,0 +1,33 @@
+import pytest
+import sys
+
+sys.path.append("../../")
+import stock_analysis
+
+
+def test_create_stock_predictor_valid_dates(
+    company_name, valid_start_date, valid_end_date
+):
+    # Given
+    stock_predictor = stock_analysis.HMMStockPredictor(
+        company=company_name,
+        start_date=valid_start_date,
+        end_date=valid_end_date,
+        future_days=0,
+    )
+    assert stock_predictor.start_date == valid_start_date
+    assert stock_predictor.end_date == valid_end_date
+    assert stock_predictor.company == company_name
+
+
+def test_create_stock_predictor_invalid_dates(
+    company_name, invalid_start_date, valid_end_date
+):
+    # Given
+    with pytest.raises(ValueError):
+        stock_predictor = stock_analysis.HMMStockPredictor(
+            company=company_name,
+            start_date=invalid_start_date,
+            end_date=valid_end_date,
+            future_days=0,
+        )
---
+++ tests/small/test_stock_predictor_helpers.py
@@ -0,0 +1,14 @@
+import pytest
+import sys
+import pandas as pd
+
+sys.path.append("../../")
+import stock_analysis
+
+
+def test_calc_mse():
+    data = {'Actual_Close': [1, 2, 3 ,4 ,5], 'Predicted_Close': [5, 4, 3, 2, 1]}
+    df = pd.DataFrame(data=data)
+    mse = stock_analysis.calc_mse(df)
+
+    assert mse == 8.0
